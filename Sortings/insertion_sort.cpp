bd073440385cf959bf36da9785c24f61f2b0f981
anshul miglani <migs.anshul@gmail.com>
2024-06-09 15:09:51 +0530

    fixed error - selection code was written in insertion sort file

--- Sortings/insertion_sort.cpp
+++ Sortings/insertion_sort.cpp
@@ -3,19 +3,7 @@
 using namespace std;
 
 vector<int> insertion_sort(vector<int> &inp){
-    int n=inp.size();
-    for(int i=0;i<n;i++){
-        int mini=INT_MAX;
-        int min_idx=-1;
-        for(int j=i;j<n;j++){
-            if(inp[j]<mini){
-                mini=inp[j];
-                min_idx=j;
-            }
-        }
-        swap(inp[i],inp[min_idx]);
-    }
-    return inp;
+    
 }
 
 int main(){
